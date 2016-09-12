2affdd64b3148d6f51e40abe947691dffee1307f
john <mywtfmp3@gmail.com>
2016-09-13 01:46:14 +0800

    customize foreman_install script

--- foreman/foreman_install.ori.sh
+++ foreman/foreman_install.ori.sh
@@ -17,26 +17,24 @@ yum -y install epel-release https://yum.theforeman.org/releases/1.12/el7/x86_64/
 
 yum -y install foreman-installer
 
-# Run foreman installer, change 
-# interface name "eth0" ==========> to your own, 
-# tftp-servername       ==========> to current it addr
+# Run foreman installer, change interface name from "eth0" to your own
 foreman-installer \
   --enable-foreman-proxy \
   --enable-foreman-plugin-remote-execution \
   --enable-foreman-plugin-templates \
   --foreman-proxy-tftp=true \
-  --foreman-proxy-tftp-servername=192.168.0.147 \
+  --foreman-proxy-tftp-servername=10.0.0.36 \
   --foreman-proxy-dhcp=true \
   --foreman-proxy-dhcp-interface=eth0 \
-  --foreman-proxy-dhcp-gateway=192.168.0.1 \
-  --foreman-proxy-dhcp-range="192.168.0.101 192.168.0.199" \
-  --foreman-proxy-dhcp-nameservers="192.168.0.123" \
+  --foreman-proxy-dhcp-gateway=10.0.0.1 \
+  --foreman-proxy-dhcp-range="10.0.0.2 10.0.0.99" \
+  --foreman-proxy-dhcp-nameservers="10.0.0.2" \
   --foreman-proxy-dns=true \
   --foreman-proxy-dns-interface=eth0 \
-  --foreman-proxy-dns-zone=wein5.com \
-  --foreman-proxy-dns-reverse=0.168.192.in-addr.arpa \
-  --foreman-proxy-dns-forwarders=192.168.0.123 \
-  --foreman-proxy-foreman-base-url=https://foreman.wein5.com
+  --foreman-proxy-dns-zone=example.com \
+  --foreman-proxy-dns-reverse=0.10.in-addr.arpa \
+  --foreman-proxy-dns-forwarders=10.0.0.2 \
+  --foreman-proxy-foreman-base-url=https://foreman-test.example.com
 
 ## if password does not work, run
 # foreman-rake permissions:reset
