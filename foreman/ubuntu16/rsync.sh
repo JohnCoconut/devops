a515c6f7470030d37157cab643784af2e82bf2d6
john <mywtfmp3@gmail.com>
2016-09-13 04:50:19 +0800

    set repo to main and restricted only

--- foreman/ubuntu16/rsync.sh
+++ foreman/ubuntu16/rsync.sh
@@ -24,14 +24,25 @@ fi
 
 rsync --recursive --times --links --hard-links \
   --stats \
+  --exclude "/pool/multiverse/*" --exclue "/pool/universe/*" \
   --exclude "Packages*" --exclude "Sources*" \
   --exclude "Release*" --exclude "InRelease" \
   --exclude "/dists/precise*" --exclude "/dists/vivid*" \
-  --exclude "/dists/wily*" --exclue "/dists/yakkety*" \
+  --exclude "/dists/wily*" --exclude "/dists/yakkety*" \
+  --exclude "/dists/lucid*" \
+  --exclude "/dists/oneiric*" \
+  --exclude "/dists/precise*" \
+  --exclude "/dists/quantal*" \
+  --exclude "/dists/raring*" \
+  --exclude "/dists/saucy*" \
+  --exclude "/dists/*-backports/*" \
+  --exclude "/dists/*-proposed/*" \
+  --exclude "/dists/*-security/*" \
+  --exclude "/dists/*-updates/*" \
   ${RSYNCSOURCE} ${BASEDIR} || fatal "First stage of sync failed."
 
-rsync --recursive --times --links --hard-links \
-  --stats --delete --delete-after \
-  ${RSYNCSOURCE} ${BASEDIR} || fatal "Second stage of sync failed."
+#rsync --recursive --times --links --hard-links \
+#  --stats --delete --delete-after \
+#  ${RSYNCSOURCE} ${BASEDIR} || fatal "Second stage of sync failed."
 
 date -u > ${BASEDIR}/project/trace/$(hostname -f)
