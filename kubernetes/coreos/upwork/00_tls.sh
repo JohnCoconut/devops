6e701a55445d7c8bcde59de058e3fc174b3637e7
john <mywtfmp3@gmail.com>
2016-10-01 19:56:29 +0800

    for upwork

---
+++ kubernetes/coreos/upwork/00_tls.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+K8S_SERVICE_IP=10.3.0.1
+MASTER_HOST=192.168.0.161
+KUBEADM_IP=192.168.0.124
+WINDOWS_IP=192.168.0.200
+
+key_dir=~/coreos_k8s/tls
+if [ ! -d $key_dir ]; then
+	mkdir -p $key_dir
+fi
+
+cd $key_dir
+
+openssl genrsa -out ca-key.pem 2048
+openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
+
+cat > openssl.cnf  << EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = kubernetes
+DNS.2 = kubernetes.default
+DNS.3 = kubernetes.default.svc
+DNS.4 = kubernetes.default.svc.cluster.local
+IP.1 = ${K8S_SERVICE_IP}
+IP.2 = ${MASTER_HOST}
+IP.3 = ${KUBEADM_IP}
+IP.4 = ${WINDOWS_IP}
+EOF
+
+openssl genrsa -out apiserver-key.pem 2048
+openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
+openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
+
+
+cat > worker-openssl.cnf << "EOF"
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = $ENV::WORKER_IP
+EOF
+
+WORKER_IP=192.168.0.164
+WORKER_FQDN=core-node1.example.com
+openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
+WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
+
+WORKER_IP=192.168.0.165
+WORKER_FQDN=core-node2.example.com
+openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
+WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
+
+openssl genrsa -out admin-key.pem 2048
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
+openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
+echo "generate certificate file admin.pfx for firefox browser, the pfx file is without password protection"
+openssl pkcs12 -export -out admin.pfx -inkey admin-key.pem -in admin.pem -passout pass:
+
+echo "the key can be found in $key_dir"
